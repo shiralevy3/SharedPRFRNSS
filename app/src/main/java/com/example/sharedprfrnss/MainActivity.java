7e3789dd2b53fc9d2d15ae970336f73f4880aa12
shiralevy3 <shiralevy333@gmail.com>
2025-01-04 14:58:24 +0200

    commit

--- app/src/main/java/com/example/sharedprfrnss/MainActivity.java
+++ app/src/main/java/com/example/sharedprfrnss/MainActivity.java
@@ -1,30 +1,51 @@
 package com.example.sharedprfrnss;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
 
+        // אחזור אלמנטים מה-XML
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        // עבודה עם SharedPreferences
+        final SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        // קריאת הערך השמור ב-SharedPreferences
+        String savedValue = sharedPreferences.getString("user_input", "###");
+        textView.setText("Hello " + savedValue);
+
+        // מאזין לכפתור שמירה
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = editText.getText().toString();
+
+                // שמירת הערך ב-SharedPreferences
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("user_input", userInput);
+                editor.apply();
+
+                // עדכון TextView
+                textView.setText("Hello " + userInput);
+            }
         });
-        EditText editText = findViewById(R.id.editText);
-        Button btn = findViewById(R.id.button);
-        
     }
 }
